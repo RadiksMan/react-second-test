d12c1f53c5588a9a44de330626995d6ad96a2695
RadiksMan <radion91@gmail.com>
2017-06-18 18:01:34 +0300

    no message

--- countdown-champ/src/index.js
+++ countdown-champ/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+const store = createStore(reducer);
 
 ReactDOM.render(
-   <App />, document.querySelector('#root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
 )
--- reminder-pro/src/constants.js
+++ reminder-pro/src/constants.js
@@ -1,10 +1,2 @@
 export const ADD_REMINDER = 'ADD_REMINDER';
 
-export const addReminder = (text) => {
-    const action = {
-        type: ADD_REMINDER,
-        text
-    }
-    console.log('action in addReminder', action);
-    return action;
-}
